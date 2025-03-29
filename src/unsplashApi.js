3adf37f312dd391076a68d557063e5875b0e85d5
PushEax <pusheax.vk@gmail.com>
2025-03-29 19:43:42 +0200

    update

---
+++ src/unsplashApi.js
@@ -0,0 +1,10 @@
+import photos from 'photos.json';
+
+const API = {
+  BASE_URL: 'https://api.unsplash.com/',
+  HEADERS: ['Accept-Version: v1', 'Authorization: Client-ID YOUR_ACCESS_KEY'],
+  client_id: 'YOUR_ACCESS_KEY',
+  authoizById: 'https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY',
+  SEARCH_URL: '/search/photos',
+  ACCESS_KEY: 'Vi2cAjvUvW27MVDBGg5H1AuSeD0n18wgJJaMXF67vcI',
+};
