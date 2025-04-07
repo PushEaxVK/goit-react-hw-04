dc8f5d6feeb3cd32d2425cb6eae9636c7cdeb0cb
PushEax <pusheax.vk@gmail.com>
2025-04-07 13:39:11 +0300

    test api

--- src/unsplashApi.js
+++ src/unsplashApi.js
@@ -1,4 +1,5 @@
-import photos from 'photos.json';
+import photos from './photos.json';
+import photosQuery from './photos_query.json';
 
 const API = {
   BASE_URL: 'https://api.unsplash.com/',
@@ -7,4 +8,15 @@ const API = {
   authoizById: 'https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY',
   SEARCH_URL: '/search/photos',
   ACCESS_KEY: 'Vi2cAjvUvW27MVDBGg5H1AuSeD0n18wgJJaMXF67vcI',
+};
+
+export const searchImages = async (query, per_page = 12) => {
+  const searchUrl = `https://api.unsplash.com/search/photos?query=${query}&client_id=${API.ACCESS_KEY}&per_page=${per_page}`;
+  const response = await fetch(searchUrl);
+  const data = await response.json();
+  return data;
+};
+
+export const fakeSearch = async (query, per_page = 12) => {
+  return photosQuery;
 };
