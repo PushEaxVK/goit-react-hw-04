269900dc406b2599abd13e32d186df4056effe53
PushEax <pusheax.vk@gmail.com>
2025-04-14 21:59:01 +0300

    start lesson 4-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,77 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import css from './App.module.css';
-import { searchImages, fakeSearch } from './services/unsplashApi';
-import LoadMoreBtn from './components/LoadMoreBtn/LoadMoreBtn';
-import SearchBar from './components/SearchBar/SearchBar';
-import ImageGallery from './components/ImageGallery/ImageGallery';
-import Loader from './components/Loader/Loader';
-import ErrorMessage from './components/ErrorMessage/ErrorMessage';
-import ImageModal from './components/ImageModal/ImageModal';
-import toast, { Toaster } from 'react-hot-toast';
+import axios from 'axios';
+// import { searchImages, fakeSearch } from './services/unsplashApi';
+// import LoadMoreBtn from './components/LoadMoreBtn/LoadMoreBtn';
+// import SearchBar from './components/SearchBar/SearchBar';
+// import ImageGallery from './components/ImageGallery/ImageGallery';
+// import Loader from './components/Loader/Loader';
+// import ErrorMessage from './components/ErrorMessage/ErrorMessage';
+// import ImageModal from './components/ImageModal/ImageModal';
+// import toast, { Toaster } from 'react-hot-toast';
 
-function App() {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const [images, setImages] = useState([]);
-  const [totalPages, setTotalPages] = useState(0);
-  const [currentPage, setCurrentPage] = useState(0);
-  const [query, setQuery] = useState('');
-  const [modalUrl, setModalUrl] = useState('');
+// function App() {
+// const [loading, setLoading] = useState(false);
+// const [error, setError] = useState(false);
+// const [images, setImages] = useState([]);
+// const [totalPages, setTotalPages] = useState(0);
+// const [currentPage, setCurrentPage] = useState(0);
+// const [query, setQuery] = useState('');
+// const [modalUrl, setModalUrl] = useState('');
 
-  const handleSearch = async (query, page) => {
-    setCurrentPage(page);
-    if (page === 1) {
-      setImages(() => {
-        return [];
-      });
-    }
-    try {
-      setLoading(true);
-      setError(false);
-      const jsonData = await searchImages(query, page);
-      if (
-        Object.keys(jsonData).length === 0 ||
-        !jsonData.results ||
-        jsonData.results.length === 0
-      ) {
-        toast.error('No results!');
-        setTotalPages(page);
-        return;
-      }
-      if (page === 1 && jsonData.total_pages) {
-        setTotalPages(jsonData.total_pages);
-      }
-      const newImages = jsonData.results.map((image) => {
-        return {
-          alt: image.alt_description,
-          blurHash: image.blur_hash,
-          color: image.color,
-          small: image.urls.small,
-          regular: image.urls.regular,
-          id: image.id,
-        };
-      });
-      if (page === 1) {
-        setImages(newImages);
-      } else {
-        setImages((prev) => {
-          return [...prev, ...newImages];
-        });
-      }
-    } catch (error) {
-      setError(true);
-    } finally {
-      setLoading(false);
-    }
-  };
+// const handleSearch = async (query, page) => {
+//   setCurrentPage(page);
+//   if (page === 1) {
+//     setImages(() => {
+//       return [];
+//     });
+//   }
+//   try {
+//     setLoading(true);
+//     setError(false);
+//     const jsonData = await searchImages(query, page);
+//     if (
+//       Object.keys(jsonData).length === 0 ||
+//       !jsonData.results ||
+//       jsonData.results.length === 0
+//     ) {
+//       toast.error('No results!');
+//       setTotalPages(page);
+//       return;
+//     }
+//     if (page === 1 && jsonData.total_pages) {
+//       setTotalPages(jsonData.total_pages);
+//     }
+//     const newImages = jsonData.results.map((image) => {
+//       return {
+//         alt: image.alt_description,
+//         blurHash: image.blur_hash,
+//         color: image.color,
+//         small: image.urls.small,
+//         regular: image.urls.regular,
+//         id: image.id,
+//       };
+//     });
+//     if (page === 1) {
+//       setImages(newImages);
+//     } else {
+//       setImages((prev) => {
+//         return [...prev, ...newImages];
+//       });
+//     }
+//   } catch (error) {
+//     setError(true);
+//   } finally {
+//     setLoading(false);
+//   }
+// };
 
-  return (
-    <div className={css.app}>
-      <SearchBar
+// return (
+// <div className={css.app}>
+// App
+{
+  /* <SearchBar
         onSubmit={(searchQuery) => {
           handleSearch(searchQuery, 1);
           setQuery(searchQuery);
@@ -85,8 +89,44 @@ function App() {
       {modalUrl !== '' && (
         <ImageModal modalUrl={modalUrl} setModal={setModalUrl} />
       )}
-      <Toaster position="top-right" />
-    </div>
+      <Toaster position="top-right" /> */
+}
+// </div>
+// );
+// }
+
+function App() {
+  const hasFetched = useRef(false);
+  const [hits, setHits] = useState([]);
+
+  useEffect(() => {
+    if (hasFetched.current) return;
+    hasFetched.current = true;
+
+    const controller = new AbortController();
+
+    axios
+      .get('https://hn.algolia.com/api/v1/search', {
+        signal: controller.signal,
+      })
+      .then((res) => {
+        setHits(res.data.hits);
+      });
+
+    return () => {
+      controller.abort;
+    };
+  }, []);
+
+  return (
+    <>
+      <div className={css.app}>App</div>
+      <ul>
+        {hits.map((item) => (
+          <li key={item.objectID}>{item.title}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
