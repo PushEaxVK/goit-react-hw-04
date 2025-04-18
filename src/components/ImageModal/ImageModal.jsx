0be966d8ac23b59c9dbe91b5e9299ea2f8564b52
pusheax <pusheax.vk@gmail.com>
2025-04-18 22:25:57 +0300

    use react-modal

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import s from './ImageModal.module.css';
+import Modal from 'react-modal';
 
 const ImageModal = ({ modalUrl, setModal }) => {
   useEffect(() => {
@@ -20,10 +21,32 @@ const ImageModal = ({ modalUrl, setModal }) => {
     <div
       className={s.modal}
       onClick={(event) => {
-        setModal('');
+        if (event.target.nodeName !== 'IMG') {
+          setModal('');
+        }
       }}
     >
-      <img src={modalUrl} />
+      <Modal
+        isOpen={modalUrl !== ''}
+        onRequestClose={() => setModal('')}
+        contentLabel='Big image'
+        style={{
+          content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)',
+            backgroundColor: 'rgba(0, 0, 0, 75%)',
+          },
+        }}
+      >
+        <div>
+          <img src={modalUrl} />
+        </div>
+        <button onClick={() => setModal('')} type='button' style={{position: 'fixed', top: '1rem', right: '1rem'}}>Close</button>
+      </Modal>
     </div>
   );
 };
